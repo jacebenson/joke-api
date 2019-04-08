e8cc91a974577e48af4d0f5439bd533ab6673092
Jace Benson <jacebenson@fico.com>
2019-04-08 16:20:18 -0500

    loaded jokes

--- express/server.js
+++ express/server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const jokes = require('../jokes');
 
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write(jokes.joke());
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));

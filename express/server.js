ac38ff58ceb5311af7eac930a9cd8610cdaf2471
Jace <jace.benson@protonmail.com>
2019-04-28 23:50:20 -0500

    Update server.js

--- express/server.js
+++ express/server.js
@@ -25,7 +25,8 @@ router.get('/many/:n', (req, res) => {
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+//app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);

407b6db5c829d87b67b7c2dabe5404e741b48a43
Jace Benson <jacebenson@fico.com>
2019-08-08 17:57:25 -0500

    working on making the page pretty

---
+++ public/custom.js
@@ -0,0 +1,30 @@
+function getRandomJoke() {
+
+  var data = null;
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+    return JSON.parse(this.responseText);
+  }
+  });
+
+  xhr.open("GET", "https://wizardly-wing-66188a.netlify.com/.netlify/functions/server");
+
+  xhr.send(data);
+}
+$(function () {
+  var loadedJoke = getRandomJoke();
+  $('#text').text(loadedJoke.text);
+  $('#author').text(loadedJoke.author);
+  $('#tweet-joke').attr('href', 'https://twitter.com/intent/tweet?hastags=dadjoke&related=joke.jace.pro&text=' + loadedJoke.text);
+  $('#new-joke').click(function () {
+    var newJoke = getRandomQuote();
+    $('#text').text(newJoke.text);
+    $('#author').text(newJoke.author);
+    $('#tweet-joke').attr('href', 'https://twitter.com/intent/tweet?hastags=dadjoke&related=joke.jace.pro&text=' + newJoke.text);
+  });
+})

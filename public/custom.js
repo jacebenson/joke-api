b022b01f5ebeff436fd2f4a598ffa57173cf5ea0
Jace Benson <jacebenson@fico.com>
2019-08-08 18:03:28 -0500

    ...

--- public/custom.js
+++ public/custom.js
@@ -1,30 +1,23 @@
+
 function getRandomJoke() {
-
   var data = null;
-
   var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
   xhr.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
     console.log(this.responseText);
-    return JSON.parse(this.responseText);
+    var loadedJoke = JSON.parse(this.responseText);
+    $('#text').text(loadedJoke.joke);
+    $('#author').text(loadedJoke.punchline);
+    $('#tweet-joke').attr('href', 'https://twitter.com/intent/tweet?hastags=dadjoke&related=joke.jace.pro&text=' + loadedJoke.joke);
+    $('#new-joke').click(function () {
+     getRandomQuote();
+    });
   }
   });
-
   xhr.open("GET", "/.netlify/functions/server");
-
   xhr.send(data);
 }
 $(function () {
-  var loadedJoke = getRandomJoke();
-  $('#text').text(loadedJoke.joke);
-  $('#author').text(loadedJoke.punchline);
-  $('#tweet-joke').attr('href', 'https://twitter.com/intent/tweet?hastags=dadjoke&related=joke.jace.pro&text=' + loadedJoke.joke);
-  $('#new-joke').click(function () {
-    var newJoke = getRandomQuote();
-    $('#text').text(newJoke.text);
-    $('#author').text(newJoke.punchline);
-    $('#tweet-joke').attr('href', 'https://twitter.com/intent/tweet?hastags=dadjoke&related=joke.jace.pro&text=' + newJoke.joke);
-  });
+  getRandomJoke();
+ 
 })

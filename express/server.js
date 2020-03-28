8e92927b1843cd32adf49f4c858c5e8a860ac0b6
Jace <638764+jacebenson@users.noreply.github.com>
2020-03-28 00:52:19 -0500

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const jokes = require('../jokes');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.writeHead(200, { 'Content-Type': 'application/json','Access-Control-Allow-Origin': '*' });
   res.write(jokes.joke());
   res.end();
 });

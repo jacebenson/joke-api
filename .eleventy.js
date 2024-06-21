5497dabe3b08b030b91a1b67b3987021c781e965
Jace <638764+jacebenson@users.noreply.github.com>
2024-06-20 23:08:32 -0500

    fixed joek stie

--- .eleventy.js
+++ .eleventy.js
@@ -29,7 +29,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: "src",
-            output: "public",
+            output: "_site",
         },
     };
 };

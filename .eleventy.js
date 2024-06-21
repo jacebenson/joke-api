1cc99fd9851acd8094469d8d64be9534070ab358
Jace <638764+jacebenson@users.noreply.github.com>
2024-06-20 23:07:51 -0500

    added jokes, fixed build

--- .eleventy.js
+++ .eleventy.js
@@ -1,60 +1,35 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const CleanCSS = require("clean-css");
+const pluginTOC = require('eleventy-plugin-nesting-toc');
+const anchor = require("markdown-it-anchor");
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(pluginRss);  
-    eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy("./src/**/*.jpg");
-    eleventyConfig.addPassthroughCopy("./src/**/*.png");
-    eleventyConfig.addPassthroughCopy("./src/**/*.webp");
-    eleventyConfig.addPassthroughCopy("./src/**/*.gif");
-    eleventyConfig.addPassthroughCopy("./src/**/*.mp4");
-    eleventyConfig.addPassthroughCopy("./src/**/*.pdf");
-    eleventyConfig.addPassthroughCopy("./src/**/*.mmd");
-    eleventyConfig.addPassthroughCopy("./src/**/*.xml");
-    eleventyConfig.addPassthroughCopy("./src/**/*.xslx");
-    eleventyConfig.addFilter("cssmin", function(code) {
-        return new CleanCSS({}).minify(code).styles;
-    });
-    // add support for syntax highlighting
+    eleventyConfig.addPlugin(pluginTOC);
     eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/images/");
+    eleventyConfig.addPassthroughCopy("./src/rest/");
+    eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
     let markdownLibrary = markdownIt({
         html: true,
         breaks: true,
         linkify: true
-      }).use(markdownItAnchor, {
-        permalink: true,
-        permalinkClass: "direct-link",
-        permalinkSymbol: "#"
-      });
-      eleventyConfig.setLibrary("md", markdownLibrary);
-
-    eleventyConfig.addPairedShortcode('details', function (content, title) {
-        let detailstag = 'details';
-        let summarystyle = arguments['2'];
-        let attributes = arguments['3'];
-        if(attributes){
-           detailstag += ' ' + attributes;
-        }
-        return `<${detailstag}><summary style="${summarystyle}"><span>${title}</span></summary>${content}</details>`;
-     });
-     eleventyConfig.addFilter("formatDate", function(value) { 
-        var d = new Date(value).toLocaleString("en-CA");
-        d.toLocaleString('en-CA',{hour12:false, timeZone: 'America/Chicago'}).replace(',','');
-        return d;
+    }).use(markdownItAnchor, {
+        permalink: anchor.permalink.headerLink()
     });
-
+    eleventyConfig.setLibrary("md", markdownLibrary);
+    eleventyConfig.addFilter("lastmodDate", function (value) {
+        // return should be yyyy-mm-dd with padded 0's for mm and dd
+        let date = new Date(value);
+        let mm = date.getMonth() + 1;
+        let dd = date.getDate();
+        return `${date.getFullYear()}-${mm.toString().padStart(2, "0")}-${dd.toString().padStart(2, "0")}`;
+    })
     return {
-        addPassthroughCopy: true,
-        markdownTemplateEngine: "njk",
-        templateFormats: ["njk", "md"],
         dir: {
-            input: "src", // html and layouts for project
-            output: "_site",
-            include: "includes",
-            data: "_data"
-        }
-    }
-}
+            input: "src",
+            output: "public",
+        },
+    };
+};
